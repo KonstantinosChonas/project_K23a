27490e46ebdfb6b9a22846b2e577d55240c6e530
arisgrigo <aris22grigo@gmail.com>
2022-10-20 17:07:45 +0300

    Testing pull/commit by adding two empty files (going to be used for input construction)

---
+++ Part1/input.c
@@ -0,0 +1,5 @@
+//
+// Created by aris on 20/10/2022.
+//
+
+#include "input.h"
---
+++ Part1/input.h
@@ -0,0 +1,8 @@
+//
+// Created by aris on 20/10/2022.
+//
+
+#ifndef PROJECTIOAN_INPUT_H
+#define PROJECTIOAN_INPUT_H
+
+#endif //PROJECTIOAN_INPUT_H
