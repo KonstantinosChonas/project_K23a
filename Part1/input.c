9174443734153961a01850b43c2e5899a4f6326a
arisgrigo <aris22grigo@gmail.com>
2022-10-29 16:24:59 +0300

    fixed some issues with hashMap

--- Part1/input.c
+++ Part1/input.c
@@ -25,7 +25,7 @@ relation* createRelation(){
     relation *newRelation = malloc(sizeof(struct relation));
 
     //newRelation->num_tuples = 20;
-    newRelation->num_tuples = rand() % 20 + 5;        //use for random number of tuples in relation
+    newRelation->num_tuples = rand() % 30 + 5;        //use for random number of tuples in relation
     newRelation->tuples = malloc(sizeof(struct tuple) * newRelation->num_tuples);
 
     for(int i = 0; i < newRelation->num_tuples; i++){
--- Part1/main.c
+++ Part1/main.c
@@ -1,15 +1,15 @@
 #include "int.h"
+#include <time.h>
 
 int main (void){
-
+    time_t t;
+    srand((unsigned)time(&t));
     relation* relR = createRelation();
     //sleep(1);                 
     relation* relS = createRelation();
-    
-    
 
 
-
+    PartitionedHashJoin(relR, relS);
 
 
 
