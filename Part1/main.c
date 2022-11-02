9043ce7f5d651078cb0ea7fc790003c5f167945f
arisgrigo <aris22grigo@gmail.com>
2022-11-02 10:49:15 +0200

    added makefile command "make run" that gets the running time

--- Part1/main.c
+++ Part1/main.c
@@ -7,16 +7,13 @@ int main (void){
     relation* relR = createRelation();
     //sleep(1);                 
     relation* relS = createRelation();
-<<<<<<< HEAD
-    
+
     
 
     result *r=PartitionedHashJoin(relR,relS);
-=======
 
->>>>>>> 378774f908513048290d696e004e242c38dc7d14
 
-    PartitionedHashJoin(relR, relS);
+//    PartitionedHashJoin(relR, relS);
 
 
 
