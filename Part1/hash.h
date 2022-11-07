b8a9c719080b40675148e5a078672b979a97767d
ko123stas <k.honas2000@gmail.com>
2022-11-07 21:14:43 +0200

    some final changes

--- Part1/hash.h
+++ Part1/hash.h
@@ -1,8 +1,6 @@
 #ifndef PROJECTIOAN_HASH_H
 #define PROJECTIOAN_HASH_H
 
-#define NEIGHBORHOOD_SIZE 4
-#define HASH_TABLE_SIZE 8000
 
 
 typedef struct payloadList{
@@ -41,3 +39,6 @@ void updateBitmapRemove(int* bitmap, int position);
 int getHash(int key, int numOfBuckets);
 
 #endif //PROJECTIOAN_HASH_H
+
+
+
