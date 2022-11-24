773f744e05242655463f41cbef98e03afdc81687
arisgrigo <aris22grigo@gmail.com>
2022-11-24 18:32:05 +0200

    query handling

--- Part2/queries.c
+++ Part2/queries.c
@@ -45,7 +45,81 @@ int parseQueries(char* queryFileName, relationInfo* usedRelations){
         projections = token;
         printf("projections: %s\n", projections);
 
+
+        /* code for relation handling */
+
+        int relationCounter = 0;
+        char tempRelations[20];
+        strcpy(tempRelations, relations);
+        printf("%s\n", tempRelations);
+        token = strtok(tempRelations, " \t");
+        relationCounter++;
+        while(token != NULL){
+            token = strtok(NULL, " \t");
+            if(token == NULL){
+                break;
+            }
+            relationCounter++;
+        }
+        int relationsArray[relationCounter];
+
+        token = strtok(relations, " \t");
+        relationsArray[0] = atoi(token);
+
+        int i = 1;
+        while(token != NULL){
+            token = strtok(NULL, " \t");
+            if(token == NULL){
+                break;
+            }
+            relationsArray[i] = atoi(token);
+            printf("%d\n", relationsArray[i]);
+            i++;
+        }
+
+        /* code for predicates handling */
+
+        printf("start of predicate handling\n");
+
+        int predicateCounter = 0;
+        char tempPredicates[20];
+        strcpy(tempPredicates, predicates);
+        printf("%s\n", predicates);
+
+        token = strtok(tempPredicates, "&");
+        predicateCounter++;
+        while(token != NULL){
+            token = strtok(NULL, "&");
+            if(token == NULL){
+                break;
+            }
+            predicateCounter++;
+        }
+        char* predicatesArray[predicateCounter+1];
+
+        token = strtok(predicates, "&");
+        predicatesArray[0] = malloc(20 * sizeof(char));
+        strcpy(predicatesArray[0], token);
+        //predicatesArray[0] = token;
+
+        i = 1;
+        while(token != NULL){
+            token = strtok(NULL, "&");
+            if(token == NULL){
+                break;
+            }
+            predicatesArray[i] = malloc(20 * sizeof(char));
+            strcpy(predicatesArray[i], token);
+            printf("%s\n", predicatesArray[i]);
+            i++;
+        }
+
+        for(i = 0; i < predicateCounter; i++){
+            free(predicatesArray[i]);
+        }
     }
+
+    printf("all done with query handling\n");
 
     fclose(fp);
     if (line){
